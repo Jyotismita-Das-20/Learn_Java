33d2c2985c4fa18072ed3c5a05ffb2fd5840d39b
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-03-02 22:14:37 +0530

    pattern print using java print instruction

---
+++ Part1/firstprogram/firstprogram.java
@@ -0,0 +1,11 @@
+package firstprogram;
+
+public class firstprogram{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("welcome to github with Jyotismita Das");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+    }
+}
